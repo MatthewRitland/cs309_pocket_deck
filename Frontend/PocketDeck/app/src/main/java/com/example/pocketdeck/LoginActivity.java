165c87232b48c5540bf72d8bb9cc2d86651a0b0e
Mack Quinn <mquinn1@iastate.edu>
2026-02-22 19:25:20 -0600

    Fix: Code now can communicate over the URL to Mockoon. Should be able to easily integrate with backend

--- Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/LoginActivity.java
+++ Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/LoginActivity.java
@@ -11,14 +11,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import com.android.volley.Request;
+import android.util.Log; // for debug msg
+import android.widget.Toast; // popup msg
+import com.android.volley.Request; //volley request from backend
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.StringRequest;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.RequestQueue; // create a request queue
+import com.android.volley.toolbox.StringRequest; //Volley that returns string
+import java.util.HashMap; // to store username and password
+import java.util.Map; // to push to UI
 
 import com.google.android.material.internal.EdgeToEdgeUtils;
 
@@ -30,7 +32,9 @@ public class LoginActivity extends AppCompatActivity {
     private Button loginButton;
     private Button signupButton;
 
-    private static final String URL_STRING_REQ = "http://10.0.2.2:3000/users/1";
+    private static final String URL_STRING_REQ = "http://10.0.2.2:3000/login"; // for macoon
+
+
     // Alternative URLs for testing purposes
     // public static final String URL_STRING_REQ = "https://2aa87adf-ff7c-45c8-89bc-f3fbfaa16d15.mock.pstmn.io/users/1";
     // public static final String URL_STRING_REQ = "http://10.0.2.2:8080/users/1";
@@ -39,7 +43,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.login_activity);
+        setContentView(R.layout.login_activity); //xml layout sheet ref
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -48,14 +52,13 @@ public class LoginActivity extends AppCompatActivity {
 
         usernameText = findViewById(R.id.login_username_txt);
         passwordText = findViewById(R.id.login_password_txt);
-        loginButton = findViewById(R.id.login_signup_button);
+        loginButton = findViewById(R.id.login_login_button);
         signupButton = findViewById(R.id.login_signup_button);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // attemptLogin();
-                // call to activity to go to try to log in
+                loginRequest(); // call to activity to go to try to log in
             }
         });
 
@@ -67,17 +70,56 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
-
-    private void attemptLogin() {
-        //method to make sure something is in the field
-        //and validate the request
-
-        return;
-    }
-
+    //to send login request to the backend
     private void loginRequest () {
-        //communicate with the backend
-        //and start the rest of the app
-        return;
+
+        // these get the text entered by the user
+        final String username = usernameText.getText().toString().trim();
+        final String password = passwordText.getText().toString().trim();
+
+        // make sure theres something entered
+        if(username.isEmpty() || password.isEmpty()){
+            Toast.makeText(LoginActivity.this, "Enter Username and Password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // upon succesful login message and activity start
+        StringRequest request = new StringRequest(Request.Method.POST, URL_STRING_REQ, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        Log.d("Success", response); //log for debugging
+
+                        //Once you login then go to the main
+                        Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(i);
+
+                    }
+                },
+                // Log in failure
+                // **THIS IS NOT INCORRECT USERNAME AND PASSWORD**
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Log.e("LOGIN_ERROR", error.toString()); //error response on login
+                        Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+        ) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Parameters for the data sent to the server
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("username", username);
+                params.put("password", password);
+                return params;
+            }
+        };
+
+        // pass this to the VolleyCommand queue
+        VolleyCommand.getInstance(this).addToRequestQueue(request);
     }
 }

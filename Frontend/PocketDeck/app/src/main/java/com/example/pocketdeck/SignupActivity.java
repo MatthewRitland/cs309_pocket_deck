1e03ab35794d53c0905306c126883e876a828407
Raine McKellar <rtmckell@iastate.edu>
2026-02-27 09:59:52 -0600

    FEAT: Added button to move to Login Screen

--- Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/SignupActivity.java
+++ Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/SignupActivity.java
@@ -1,5 +1,6 @@
 package com.example.pocketdeck;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,16 +10,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Toast;
+
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class SignupActivity extends AppCompatActivity{
 
     private EditText nameInput, passwordInput, confirmPasswordInput;
-    private Button continueButton;
+    private Button continueButton, loginLinkButton;
+
+    // HTTP request URLs
+    private static final String URL_USER_CREATE = "temp/signup"; // Temp, change later
 
     @Override
     protected void onCreate(Bundle savedInstancesState)
@@ -35,33 +43,34 @@ public class SignupActivity extends AppCompatActivity{
         continueButton = findViewById(R.id.continueButton);
         continueButton.setOnClickListener(new View.OnClickListener() {
             @Override
+            public void onClick(View v) { create_user_request(); }
+        });
+
+        loginLinkButton = findViewById(R.id.loginLinkButton);
+        loginLinkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                String username = nameInput.getText().toString();
-                String password = passwordInput.getText().toString();
-                String confirm = confirmPasswordInput.getText().toString();
-
-                if (username.isEmpty() || password.isEmpty() || confirm.isEmpty()) {
-                    // An input is empty
-                    Toast.makeText(getApplicationContext(), "Please fill out all input fields", Toast.LENGTH_LONG).show();
-                }
-
-                if (!password.equals(confirm)) {
-                    // Does not match up, invalid.
-                    Toast.makeText(getApplicationContext(), "Passwords do not match", Toast.LENGTH_LONG).show();
-                } else {
-                    // Check if user with username exists
-                    // TEMP: Add call to check username
-                    boolean user_exists = false;
-                    if (user_exists) {
-                        Toast.makeText(getApplicationContext(), "A user with that username already exists", Toast.LENGTH_LONG).show();
-                        return;
-                    }
-
-                    // Passwords match and username is unique
-                    // Make call to create user.
-
-                }
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
             }
         });
+    }
+
+    private void create_user_request() {
+        String username = nameInput.getText().toString();
+        String password = passwordInput.getText().toString();
+        String confirm = confirmPasswordInput.getText().toString();
+
+        if (username.isEmpty() || password.isEmpty() || confirm.isEmpty()) {
+            // An input is empty
+            Toast.makeText(getApplicationContext(), "Please fill out all input fields", Toast.LENGTH_LONG).show();
+        }
+
+        if (!password.equals(confirm)) {
+            // Does not match up, invalid.
+            Toast.makeText(getApplicationContext(), "Passwords do not match", Toast.LENGTH_LONG).show();
+        } else {
+            // Send request to create user
+        }
     }
 }

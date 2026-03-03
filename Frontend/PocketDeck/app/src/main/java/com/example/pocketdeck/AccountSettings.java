7ec29c662267922b47a54bcb3415127529b8845a
Raine McKellar <rtmckell@iastate.edu>
2026-03-03 09:33:34 -0600

    REFACTOR: Added some skeleton code for Account Settings

--- Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/AccountSettings.java
+++ Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/AccountSettings.java
@@ -1,6 +1,9 @@
 package com.example.pocketdeck;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AccountSettings extends AppCompatActivity {
+
+    // Text Input and Display
+    private EditText usernameInput;
+    private EditText currentPasswordInput, newPasswordInput, confirmPasswordInput;
+    private TextView userIdLabel;
+
+    // Buttons
+    private Button confirmButton, backButton, logoutButton;
+    private Button inputRevealButton, idRevealButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,78 @@ public class AccountSettings extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        updateDisplay();
+    }
+
+    /**
+     * Updates the components of the page to represent current user settings.
+     */
+    private void updateDisplay() {
+        // Update all the components to represent present user settings.
+    }
+
+    private boolean confirmChoices() {
+        // Do checks
+
+        // Do communication
+        return false;
+    }
+
+    enum InputResult {
+        VALID,
+        INVALID,
+        BLANK
+    }
+
+    private boolean handlePassword() {
+        String currentPassword = currentPasswordInput.getText().toString().trim();
+        String newPassword = newPasswordInput.getText().toString().trim();
+        String confirmPassword = confirmPasswordInput.getText().toString().trim();
+        InputResult validInput = validPasswordInput(currentPassword, newPassword, confirmPassword);
+
+        if (validInput == InputResult.INVALID) {
+            // TODO: Invalid password toast
+            return false;
+        }
+
+        if (validInput == InputResult.VALID) {
+            if (currentPassword.equals(newPassword)) {
+                // TODO : Toast
+                return false;
+            }
+            // TODO: Send new password request to server
+            // TODO: Get user_id
+        }
+
+        return true;
+    }
+
+    /**
+     * Checks the new password fields for whether or not the password should be changed.
+     * @return The checks result, either that input is BLANK, INVALID in some way or VALID
+     */
+    private InputResult validPasswordInput(String currentPassword, String newPassword, String confirmPassword) {
+        // All inputs are BLANK, password should not be changed.
+        if (currentPassword.isEmpty() && newPassword.isEmpty() && confirmPassword.isEmpty()) {
+            return InputResult.BLANK;
+        }
+
+        // Any but not all inputs are blank, password changing input is INVALID.
+        if (currentPassword.isEmpty() || newPassword.isEmpty() || confirmPassword.isEmpty()) {
+            return InputResult.INVALID;
+        }
+
+        // Confirmation field doesn't match the new password, input INVALID.
+        if (!newPassword.equals(confirmPassword)) return InputResult.INVALID;
+
+        // All inputs are (technically) valid.
+        return InputResult.VALID;
+    }
+
+    private class passwordBox {
+        private String currentPassword;
+        private String newPassword;
+        private String confirmPassword;
     }
 }

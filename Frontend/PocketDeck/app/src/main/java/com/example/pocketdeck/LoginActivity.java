7c7f5ae4a791cc5a66907810670df3aa174de6ce
Raine McKellar <rtmckell@iastate.edu>
2026-03-06 14:01:22 -0600

    FIX: Changed response processing

--- Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/LoginActivity.java
+++ Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/LoginActivity.java
@@ -96,40 +96,40 @@ public class LoginActivity extends AppCompatActivity {
 
                         //NEW ADDED FOR JSON
                         try {
-                            //create the object and read the value returned
-                            JSONObject json = new JSONObject(response);
+//                            //create the object and read the value returned
+//                            JSONObject json = new JSONObject(response);
+//
+//                            String message = json.optString("message");
+//                            boolean loginSuccess = json.optBoolean("success");
+//
+//                            //this happens if the login success is true
+//                            //EDIT: crate a flag using SharedPreferences to save login data even when the app is closed
+//                            if(loginSuccess){
+                            //Create json object to store all user info in the shared preferences
+                            JSONObject userInfo = new JSONObject(response);
 
-                            String message = json.optString("message");
-                            boolean loginSuccess = json.optBoolean("success");
+                            int userID = userInfo.optInt("id");
+                            String uname = userInfo.optString("username");
+                            String status = userInfo.optString("userStatus");
+                            //create a shared preference that saves data to "userLoggedIn". MODE_Private means that
+                            //only the app can use this.
+                            //Look at this for more info. https://www.geeksforgeeks.org/android/shared-preferences-in-android-with-examples/
+                            //I used this page and some others to figure this out
+                            //Save the flag here when you sign in so that I can use it in main for the play button logic
+                            SharedPreferences preferences = getSharedPreferences("userLoggedInCheck", MODE_PRIVATE);
+                            preferences.edit().putBoolean("isLoggedIn", true).apply();
+                            preferences.edit().putInt("userID", userID).apply();
+                            preferences.edit().putString("username", uname).apply();
+                            preferences.edit().putString("status", status).apply();
 
-                            //this happens if the login success is true
-                            //EDIT: crate a flag using SharedPreferences to save login data even when the app is closed
-                            if(loginSuccess){
-                                //Create json object to store all user info in the shared preferences
-                                JSONObject userInfo = json.getJSONObject("user");
+                            Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
 
-                                int userID = userInfo.getInt("id");
-                                String uname = userInfo.getString("username");
-                                String status = userInfo.getString("userStatus");
-                                //create a shared preference that saves data to "userLoggedIn". MODE_Private means that
-                                //only the app can use this.
-                                //Look at this for more info. https://www.geeksforgeeks.org/android/shared-preferences-in-android-with-examples/
-                                //I used this page and some others to figure this out
-                                //Save the flag here when you sign in so that I can use it in main for the play button logic
-                                SharedPreferences preferences = getSharedPreferences("userLoggedInCheck", MODE_PRIVATE);
-                                preferences.edit().putBoolean("isLoggedIn", true).apply();
-                                preferences.edit().putInt("userID", userID).apply();
-                                preferences.edit().putString("username", uname).apply();
-                                preferences.edit().putString("status", status).apply();
-
-                                Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
-
-                                Intent i = new Intent(LoginActivity.this, MainActivity.class);
-                                startActivity(i);
-                            } else {
-                                //if the backend says login failed
-                                Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
-                            }
+                            Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(i);
+//                            } else {
+//                                //if the backend says login failed
+//                                Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
+//                            }
                         } catch (JSONException e){
                             //if the backend doesnt send a json this will happen
                             Log.e("JSON_ERROR", e.toString());

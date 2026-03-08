030b87950c05472c8955bc72fc4ec2fd3f1ec42d
mattrit <mattrit@iastate.edu>
2026-03-08 03:55:57 -0500

    fix: fixed a bug in update

--- Backend/4_sh_2_backend_project/src/main/java/onetoone/Users/UserRepository.java
+++ Backend/4_sh_2_backend_project/src/main/java/onetoone/Users/UserRepository.java
@@ -13,7 +13,22 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     // returns true or false if username is taken/exists
     boolean existsByUsername(String username);
 
+    // looks to see if there exists a username that DOES NOT have the given id
+    boolean existsByUsernameAndIdNot(String username, int id);
+
     @Transactional
     void deleteById(int id);
 
 }
+
+
+/* Spring Data JPA follows the format: (with appropriate Capitalization)
+
+[action prefix] + [property] +      [keyword]         + [glue] + ...(repeat)...
+   existsBy        username            NOT               AND
+   findBy          password        "" = Is = Equals      OR
+   deleteBy        status            LessThan
+   countBy          ...              IsNull
+     ...                           IgnoreCase
+                                     ^...^
+*/

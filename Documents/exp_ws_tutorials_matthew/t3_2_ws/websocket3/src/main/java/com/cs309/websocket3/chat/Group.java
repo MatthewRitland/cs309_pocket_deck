a89486d5496ad7cc50fc9cdb2bf626ecbb4d118a
mattrit <mattrit@iastate.edu>
2026-03-27 22:22:38 -0500

    feat: very basic framework of what to work on next for messaging feature.

---
+++ Documents/exp_ws_tutorials_matthew/t3_2_ws/websocket3/src/main/java/com/cs309/websocket3/chat/Group.java
@@ -0,0 +1,6 @@
+package com.cs309.websocket3.chat;
+
+// should involve a collection of users that are in a chat (and or thread?)
+// may need a list or array per group object
+public class Group {
+}
---
+++ Documents/exp_ws_tutorials_matthew/t3_2_ws/websocket3/src/main/java/com/cs309/websocket3/chat/GroupMembership.java
@@ -0,0 +1,6 @@
+package com.cs309.websocket3.chat;
+
+// should involve the membship status of users. Need to figure out how to
+// get websockets into our backend folder without breaking stuff first, though.
+public class GroupMembership {
+}

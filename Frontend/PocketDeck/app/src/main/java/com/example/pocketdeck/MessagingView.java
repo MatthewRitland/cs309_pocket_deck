bb07b1c1b5a5a21eee36c5ca53d7181ff90cde08
Raine McKellar <rtmckell@iastate.edu>
2026-04-01 11:40:03 -0500

    FEAT: Added base class for messaging activity along with basic screen and RecyclerView dependancies.

---
+++ Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/MessagingView.java
@@ -0,0 +1,10 @@
+package com.example.pocketdeck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MessagingView extends AppCompatActivity {
+
+    protected void onCreate() {
+
+    }
+}

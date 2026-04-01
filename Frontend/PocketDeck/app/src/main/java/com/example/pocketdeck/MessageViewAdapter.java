d082c6542ccc807f76095e7cd69198f35eb58ad5
Raine McKellar <rtmckell@iastate.edu>
2026-04-01 15:58:43 -0500

    FEAT: Branched Message class into seperate file

--- Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/MessageViewAdapter.java
+++ Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/MessageViewAdapter.java
@@ -44,30 +44,12 @@ public class MessageViewAdapter extends RecyclerView.Adapter<MessageViewAdapter.
         holder.messageText.setText(message.getUsername());
     }
 
+    public void addNewMessage(Message newMessage) {
+        messageList.add(newMessage);
+    }
+
     @Override
     public int getItemCount() {
         return messageList.size();
-    }
-
-    public class Message {
-        // Display name of the sender
-        private String senderName;
-        // Message contents
-        private String message;
-
-        public Message(String senderName, String message) {
-            // Construct message object
-            this.senderName = senderName;
-            this.message = message;
-            // Create UI object
-        }
-
-        public String getUsername() {
-            return senderName;
-        }
-
-        public String getMessage() {
-            return message;
-        }
     }
 }
--- Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/MessagingView.java
+++ Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/MessagingView.java
@@ -3,11 +3,13 @@ package com.example.pocketdeck;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MessagingView extends AppCompatActivity {
@@ -19,7 +21,22 @@ public class MessagingView extends AppCompatActivity {
         setContentView(R.layout.activity_messaging);
         messageView = findViewById(R.id.MessagesView);
 
+        List<Message> messages = new ArrayList<Message>();
+        // Demo lines
+        messages.add(new Message("Demo User","I am speaking presently"));
+        messages.add(new Message("Non-user", "How did I get here?"));
+        messages.add(new Message("I AM THE YAPPER", "HERPADERPADOOBEEDOODOOHAHA I call" +
+                " upon the great powers beyond the veil to cast lightning storm on your pathetic being. It" +
+                " would really be funny, I think. I've written in this single textbox more than I ever have" +
+                " before in my dumb life. Are you proud of me, father?"));
+
+        messageView.setLayoutManager(new LinearLayoutManager(this));
+
+        MessageViewAdapter messageAdapter = new MessageViewAdapter(messages);
+        messageView.setAdapter(messageAdapter);
         // Get messages array from server / websocket
         // Update display
     }
+
+
 }

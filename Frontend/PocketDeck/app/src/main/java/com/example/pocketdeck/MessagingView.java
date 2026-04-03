120dd744e3b0072d616fb4c9ec927274ffe948ba
Raine McKellar <rtmckell@iastate.edu>
2026-04-03 15:20:21 -0500

    REFACTOR: I just want to switch branches, I didn't change anything.

--- Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/MessagingView.java
+++ Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/MessagingView.java
@@ -80,7 +80,7 @@ public class MessagingView extends AppCompatActivity implements WebSocketListene
         // TODO : Code
         /* Load previous messages from this group */
     }
-    
+
     @Override
     public void onWebSocketMessage(String message) {
         // Run on UI

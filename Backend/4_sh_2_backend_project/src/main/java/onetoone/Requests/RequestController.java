276d9ed7b15704c8c8f44d094ed058f2f475c1e4
Austin Donald <adonald@iastate.edu>
2026-04-03 20:32:35 -0500

    doc: added documentation to RequestController

--- Backend/4_sh_2_backend_project/src/main/java/onetoone/Requests/RequestController.java
+++ Backend/4_sh_2_backend_project/src/main/java/onetoone/Requests/RequestController.java
@@ -1,6 +1,13 @@
 package onetoone.Requests;
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import onetoone.CardGames.CardGame;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,22 +19,52 @@ public class RequestController {
     @Autowired
     RequestRepository requestRepo;
 
+    @Operation(summary = "lists all Requests", description = "returns a list from the database of all Request objects")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successfully returned a list of all Requests",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = CardGame.class))
+                    })
+    })
     @GetMapping("/request")
     List<Request> getAllRequests () {
         return requestRepo.findAll();
     }
-    @GetMapping("/request/requested/{id}")
-    Request getRequestsByRequestedAndRequester (@PathVariable int requestedId, @PathVariable int requesterId) {
+
+    @Operation(summary = "finds one request", description = "returns a list a Request from the database based on the requested and requester ids")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successfully returned a Request",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = CardGame.class))
+                    })
+    })
+    @GetMapping("/request/requested/{id}/{id}")
+    Request getRequestsByRequestedAndRequester (@Parameter(description = "id of requested user")@PathVariable int requestedId, @Parameter(description = "id of requester user")@PathVariable int requesterId) {
         return requestRepo.findByRequestedIdAndRequesterId(requestedId, requesterId);
     }
 
+    @Operation(summary = "finds one request", description = "returns a list a Request from the database based on the Request's id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successfully returned a Request",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = CardGame.class))
+                    })
+    })
     @GetMapping("/request/{id}")
-    Request getRequestById (@PathVariable int id) {
+    Request getRequestById (@Parameter(description = "id of Request")@PathVariable int id) {
         return requestRepo.findById(id);
     }
 
+    @Operation(summary = "creates a Request", description = "creates a Request and stores it in the database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successfully created a Request",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = CardGame.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "missing one or two users", content = @Content)
+    })
     @PostMapping("/request")
-    Request createRequest (@RequestBody Request req) {
+    Request createRequest (@Parameter(description = "Request object to save to the databse")@RequestBody Request req) {
         if (req == null || req.getRequested() == null || req.getRequester() == null) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing a user");
         }
@@ -36,8 +73,16 @@ public class RequestController {
         return req;
     }
 
+    @Operation(summary = "updates one Request", description = "takes a Request object to update a Request from the database and returns the updated Request")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successfully updated a Request",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = CardGame.class))
+                    }),
+            @ApiResponse(responseCode = "404", description = "failed to find a Request", content = @Content)
+    })
     @PutMapping("/request/{id}")
-    Request updateRequest (@RequestBody Request req, @PathVariable int id) {
+    Request updateRequest (@Parameter(description = "Request object to update with")@RequestBody Request req, @Parameter(description = "id of Request")@PathVariable int id) {
         Request current = requestRepo.findById(id);
         if (current == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Request not found");
@@ -51,8 +96,15 @@ public class RequestController {
         return current;
     }
 
+    @Operation(summary = "deletes one Request", description = "deletes a Request and returns a message")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successfully deleted a Request",
+                    content = {@Content(mediaType="application/json",
+                            schema = @Schema(implementation = CardGame.class))
+                    })
+    })
     @DeleteMapping("request/{id}")
-    String deleteRequest (@PathVariable int id) {
+    String deleteRequest (@Parameter(description = "id of Request")@PathVariable int id) {
         Request toBeDeleted = requestRepo.findById(id);
         if (toBeDeleted == null) {
             return "{\"message\":\"failure\"}";

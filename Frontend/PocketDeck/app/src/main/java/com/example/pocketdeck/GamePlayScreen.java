3f251a588188d354b61775a1f778876e1f7f0f50
Mack Quinn <mquinn1@iastate.edu>
2026-04-04 21:59:17 -0500

    Feature: Started gameplay screen

--- Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/GamePlayScreen.java
+++ Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/GamePlayScreen.java
@@ -11,8 +11,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import org.java_websocket.handshake.ServerHandshake;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class GamePlayScreen extends AppCompatActivity {
+
+    //private static final String WS_URL = "ws://coms-3090-025.class.las.iastate.edu:8080/";
+
+    private WebsocketManager webSocketManager;
+
+    private TextView statusText;
+    private Button moveButton1;
+    private Button moveButton2;
+    private Button moveButton3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +40,34 @@ public class GamePlayScreen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
+
+        statusText = findViewById(R.id.statusText);
+        moveButton1 = findViewById(R.id.moveButton1);
+        moveButton2 = findViewById(R.id.moveButton2);
+        moveButton3 = findViewById(R.id.moveButton3);
+
+        statusText.setText("Connecting...");
+        moveButton1.setText("Move 1");
+        moveButton2.setText("Move 2");
+        moveButton3.setText("Leave");
+
+        moveButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        moveButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        moveButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        }
 }

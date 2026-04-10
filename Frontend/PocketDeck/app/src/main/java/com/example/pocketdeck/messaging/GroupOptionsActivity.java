299467af61ef8f7620439770c8906846c8a06f1c
Raine McKellar <rtmckell@iastate.edu>
2026-04-10 14:05:12 -0500

    REFACTOR: Minor Code Change

--- Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/messaging/GroupOptionsActivity.java
+++ Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/messaging/GroupOptionsActivity.java
@@ -1,8 +1,13 @@
 package com.example.pocketdeck.messaging;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GroupOptionsActivity extends AppCompatActivity {
 
-    
+    @Override
+    protected void onCreate(Bundle savedInstancesState) {
+        super.onCreate(savedInstancesState);
+    }
 }

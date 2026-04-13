6a4efa964b6f7b8b0fe894977ce9a80f8919dcf9
Raine McKellar <rtmckell@iastate.edu>
2026-04-13 13:38:44 -0500

    FIX: WebSocketListener to WebsocketListener

--- Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/WebsocketListener.java
+++ Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/WebsocketListener.java
@@ -2,12 +2,30 @@ package com.example.pocketdeck;
 
 import org.java_websocket.handshake.ServerHandshake;
 
-    public interface WebsocketListener {
-        void onWebSocketOpen(ServerHandshake handshakedata);
+public interface WebsocketListener {
 
-        void onWebSocketMessage(String message);
+    /**
+     * Called on successful connection to the websocket.
+     *
+     * @param handshakeData Information on the Servers Handshake
+     */
+    void onWebSocketOpen(ServerHandshake handshakeData);
 
-        void onWebSocketClose(int code, String reason, boolean remote);
+    /**
+     * Called when a WebSocket message is received.
+     *
+     * @param message The received WebSocket message.
+     */
+    void onWebSocketMessage(String message);
 
-        void onWebSocketError(Exception ex);
-    }
+    /**
+     * Called when a WebSocket has been closed
+     *
+     * @param code Status code indicating reason for closure.
+     * @param reason String explaining the reason for closure in human-readable terms.
+     * @param remote Indicates whether the closure was from the remote endpoint.
+     */
+    void onWebSocketClose(int code, String reason, boolean remote);
+
+    void onWebSocketError(Exception ex);
+}

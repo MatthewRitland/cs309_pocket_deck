bbf1d28cb73462298eb73e6bf10c364a2161fa5a
mattrit <mattrit@iastate.edu>
2026-04-19 22:52:22 -0500

    test: testing CI again

--- Backend/4_sh_2_backend_project/src/main/java/onetoone/GameLobby/GameLobby.java
+++ Backend/4_sh_2_backend_project/src/main/java/onetoone/GameLobby/GameLobby.java
@@ -1,4 +1,44 @@
 package onetoone.GameLobby;
 
+import jakarta.persistence.*;
+import onetoone.Users.User;
+import onetoone.CardGames.CardGame;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+
+// this class exists to represent a game lobby where users will gather before starting a game
+@Entity
 public class GameLobby {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+
+    // there are many users in one game lobby
+    @OneToMany
+    @JoinColumn(nullable = false)
+    private List<User> usersList; //todo should this be a user list in this class, or leave that to a
+    // group membership like in messaging?
+
+    // there is one card game for one game lobby
+    @OneToOne
+    @JoinColumn(nullable = false)
+    private CardGame cardGame;
+
+    private boolean isInviteOnly;
+
+    public GameLobby() {
+        // figure out rest of logic.
+    }
+
+
+    // =============================== Getters and Setters for each field ================================== //
+
+    public int getId() { return this.id; }
 }
+
+
--- Backend/4_sh_2_backend_project/src/main/java/onetoone/GameLobby/GameLobbyRepository.java
+++ Backend/4_sh_2_backend_project/src/main/java/onetoone/GameLobby/GameLobbyRepository.java
@@ -1,4 +1,14 @@
 package onetoone.GameLobby;
 
-public interface GameLobbyRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface GameLobbyRepository extends JpaRepository<GameLobby, Integer> {
+    GameLobby findById(int id);
+
+    @Transactional
+    void deleteById(int id);
+
 }

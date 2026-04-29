0c2aacfc6e6601d8672da333ead181740cc9be0f
Raine McKellar <rtmckell@iastate.edu>
2026-04-29 14:14:34 -0500

    REFACTOR: Switched Volley reference

--- Frontend/PocketDeck/app/build.gradle.kts
+++ Frontend/PocketDeck/app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("com.android.volley:volley:1.2.1")
+    implementation(libs.volley)
     androidTestImplementation(platform(libs.compose.bom))
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)

270d31329a907377a6b01349d3dd3ecafbba0c39
Raine McKellar <rtmckell@iastate.edu>
2026-05-01 13:37:19 -0500

    FEAT: Basic ready functionality

--- Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/LobbyScreen.java
+++ Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/LobbyScreen.java
@@ -1,11 +1,19 @@
 package com.example.pocketdeck;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class LobbyScreen extends AppCompatActivity {
+
+    private Button inviteButton, readyButton, leaveButton;
+    private RecyclerView activeUserList;
+
+    private boolean isHost, allReadied, localUserReady;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +21,40 @@ public class LobbyScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
 
-        /* Specialty */
+        /* Get UI elements */
+        inviteButton = findViewById(R.id.lobby_inviteButton);
+        readyButton = findViewById(R.id.lobby_readyButton);
+        leaveButton = findViewById(R.id.lobby_leaveButton);
+
+        /* Initialize UI elements */
+        initReadyButton();
+    }
+
+    private void allUsersReady() {
+        if (!isHost) return;
+        allReadied = true;
+        readyButton.setText("Begin");
+    }
+
+    private void userUnreadied() {
+        if(!allReadied) return;
+        allReadied = false;
+
+    }
+
+    private void initReadyButton() {
+        updateReadyBtnDisplay();
+        readyButton.setOnClickListener(v -> onReadyClicked());
+    }
+
+    private void updateReadyBtnDisplay() {
+        if (localUserReady) { readyButton.setText("Ready"); }
+        else { readyButton.setText("Unready"); }
+    }
+
+    private void onReadyClicked() {
+        localUserReady = !localUserReady;
+        // TODO: Send input out to other users
+        updateReadyBtnDisplay();
     }
 }

ab7291969dcf77f01d4b8a1f27f0bbe9c5874e28
Raine McKellar <rtmckell@iastate.edu>
2026-05-01 13:24:34 -0500

    FEAT: Lobby script initialization

---
+++ Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/LobbyScreen.java
@@ -0,0 +1,18 @@
+package com.example.pocketdeck;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LobbyScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        /* Standard Initialization */
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        /* Specialty */
+    }
+}

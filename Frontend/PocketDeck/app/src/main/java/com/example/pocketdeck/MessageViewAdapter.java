284b97ab9066b23327633c7d5cc64142de2e161f
Raine McKellar <rtmckell@iastate.edu>
2026-05-06 14:24:55 -0500

    FIX: Message UI improvement

--- Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/MessageViewAdapter.java
+++ Frontend/PocketDeck/app/src/main/java/com/example/pocketdeck/MessageViewAdapter.java
@@ -45,7 +45,11 @@ public class MessageViewAdapter extends RecyclerView.Adapter<MessageViewAdapter.
         UserUtilities userUtils = new UserUtilities(holder.itemView.getContext());
         if (message.getUsername().equals(userUtils.getSavedUsername())) {
             holder.usernameText.setGravity(Gravity.LEFT );
-        } else { holder.usernameText.setGravity(Gravity.RIGHT ); }
+            holder.messageText.setGravity(Gravity.LEFT);
+        } else {
+            holder.usernameText.setGravity(Gravity.RIGHT );
+            holder.messageText.setGravity(Gravity.RIGHT );
+        }
         holder.messageText.setText(message.getMessage());
     }
 
